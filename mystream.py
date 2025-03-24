773ed7037c3dabb613d532e1251fa94ca0b83c40
Natalia Bedoya <42577095+tolondra@users.noreply.github.com>
2025-03-24 15:23:14 +0000

    Test 	modified:   mystream.py

--- mystream.py
+++ mystream.py
@@ -1,3 +1,21 @@
 import streamlit as st
 
-st.write('Hello Daniel! We could deploy the WUI here')
+
+
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+from functions import *
+from helper_Functions import *
+
+
+if __name__ == "__main__":
+    # """
+    # Main function to run the Streamlit app. It sets up the sidebar, imports data, creates sliders,
+    # filters the data, and provides options for visualizing and downloading the data.
+    # """
+    # Set the title for the sidebar
+    st.sidebar.title("Hello Daniel! We could deploy the WUI here")
+    st.sidebar.write("")
+
